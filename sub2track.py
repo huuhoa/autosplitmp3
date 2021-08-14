ef22ff53f12a172f5619b3bec5e8606cdc2735b8
Nguyen Huu Hoa <huuhoa@gmail.com>
2021-08-14 20:34:35 +0700

    add requirements.txt to install correct autosub

--- sub2track.py
+++ sub2track.py
@@ -30,20 +30,14 @@ def append_track(tracks, start, line_timing, filename, current_chapter):
     print('track: %s' % track)
 
 
-def main():
-    """create track list file from subtitle"""
-
-    # record command line args
-    subtitle, track_list, prefix = parse_arg()
-
+def read_srt(src):
     tracks = []
     # read each line of the track list and split into start, end, name
-    with open(subtitle, 'r') as f:
-        start = '00:00:00'
-        current_chapter = ''
-        current_file = ''
+    with open(src, 'r') as f:
         line_index = 0
-        filename = ''
+        seg_index = 0
+        seg_time = ''
+        seg_content = ''
         for line in f:
             # skip comment
             if line.startswith('#'):
@@ -58,35 +52,108 @@ def main():
             line_index += 1
             # skip line numbering
             if line_index == 1:
+                seg_index = line.strip()
                 continue
             if line_index == 2: # subtitle time start-end
-                line_timing, _, _ = line.strip().split(',')
+                seg_time = line.strip()
                 continue
 
             # line_index == 3: - subtitle content
-            if not line.startswith('Chapter'):
-                continue
-
-            # encounter first chapter, should not append track since it is just the begining
-            if len(current_chapter) <= 1:
-                filename, current_chapter = get_filename_title(prefix, line)
-                continue
-
-            # begining of new chapter, should record current chapter 
-            append_track(tracks, start, line_timing, filename, current_chapter)
-
-            # starting new chapter
-            filename, current_chapter = get_filename_title(prefix, line)
-            start = line_timing
-
+            seg_content = line.strip()
+            tracks.append((seg_index, seg_time, seg_content))
+            line_index = 0
         # end for
 
-        # last chapter
-        if len(current_chapter) > 1:
-            append_track(tracks, start, line_timing, filename, current_chapter)
+    return tracks
+
+
+def transform_tracks(srt_segments, prefix):
+    start = '00:00:00'
+    current_chapter = ''
+    filename = ''
+    tracks = []
+    for _, seg_time, content in srt_segments:
+        if not content.startswith('Chapter '):
+            continue
+        line_timing, _, _ = seg_time.split(',')
+
+        # encounter first chapter, should not append track since it is just the begining
+        if len(current_chapter) <= 1:
+            filename, current_chapter = get_filename_title(prefix, "begin")
+
+        # begining of new chapter, should record current chapter
+        append_track(tracks, start, line_timing, filename, current_chapter)
+
+        # starting new chapter
+        filename, current_chapter = get_filename_title(prefix, content)
+        start = line_timing
+
+    # end for
+
+    # last chapter
+    if len(current_chapter) > 1:
+        _, seg_time, _ = srt_segments[-1]
+        line_timing, _, _ = seg_time.split(',')
+        append_track(tracks, start, line_timing, filename, current_chapter)
+    return tracks
+
+
+def main():
+    """create track list file from subtitle"""
+
+    # record command line args
+    subtitle, track_list, prefix = parse_arg()
+    srt_segments = read_srt(subtitle)
+    tracks = transform_tracks(srt_segments, prefix)
+    # # read each line of the track list and split into start, end, name
+    # with open(subtitle, 'r') as f:
+    #     start = '00:00:00'
+    #     current_chapter = ''
+    #     current_file = ''
+    #     line_index = 0
+    #     filename = ''
+    #     for line in f:
+    #         # skip comment
+    #         if line.startswith('#'):
+    #             continue
+
+    #         # empty line
+    #         if len(line) <= 1:
+    #             line_index = 0
+    #             continue
+
+    #         # not empty lines
+    #         line_index += 1
+    #         # skip line numbering
+    #         if line_index == 1:
+    #             continue
+    #         if line_index == 2: # subtitle time start-end
+    #             line_timing, _, _ = line.strip().split(',')
+    #             continue
+
+    #         # line_index == 3: - subtitle content
+    #         if not line.startswith('Chapter '):
+    #             continue
+
+    #         # encounter first chapter, should not append track since it is just the begining
+    #         if len(current_chapter) <= 1:
+    #             filename, current_chapter = get_filename_title(prefix, line)
+    #             continue
+
+    #         # begining of new chapter, should record current chapter
+    #         append_track(tracks, start, line_timing, filename, current_chapter)
+
+    #         # starting new chapter
+    #         filename, current_chapter = get_filename_title(prefix, line)
+    #         start = line_timing
+
+    #     # end for
+
+    #     # last chapter
+    #     if len(current_chapter) > 1:
+    #         append_track(tracks, start, line_timing, filename, current_chapter)
 
     write_track_list(tracks, track_list)
-    return None
 
 
 if __name__ == '__main__':
